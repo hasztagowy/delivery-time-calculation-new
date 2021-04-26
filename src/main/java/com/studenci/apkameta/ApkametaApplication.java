3719635342dfd2f2edeb2bb40b8998e7ea44e5e5
miszorkappa123@gmail.com <miszorkappa123@gmail.com>
2021-04-26 17:53:50 +0200

    witam

---
+++ src/main/java/com/studenci/apkameta/ApkametaApplication.java
@@ -0,0 +1,13 @@
+package com.studenci.apkameta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApkametaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApkametaApplication.class, args);
+    }
+
+}
