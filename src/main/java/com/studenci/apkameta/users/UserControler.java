3f0cbaa4e90c995cc15eff53081c2fd29e705f7a
mihau1211 <mihau1211@users.noreply.github.com>
2021-05-11 22:18:57 +0200

    Update UserControler.java

--- src/main/java/com/studenci/apkameta/users/UserControler.java
+++ src/main/java/com/studenci/apkameta/users/UserControler.java
@@ -1,5 +1,7 @@
 package com.studenci.apkameta.users;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class UserControler {
+
+    @Autowired
+    UserDto userDto;
 
     @Autowired
     UserServiceImpl userService;
@@ -24,11 +31,26 @@ public class UserControler {
         return "wuitam";
     }
 
-
-
     @PostMapping("/authorize")
-    public String authorizeUser(@RequestBody LoginDto user){
-        userService.login(user);
-        return "wuitam";
+    public String login(@RequestBody LoginDto user) {
+
+        if(userService.login(user)) {
+            User user1= userDto.findTopByUsername((user.getUsername()));
+            Long now = System.currentTimeMillis();
+            String token =  Jwts.builder()
+                    .setSubject(user.getUsername()) // 1
+                    .claim("role", user1.getRole()) // 2
+                    .setIssuedAt(new Date(now)) // 3
+                    .setExpiration(new Date(now + 10000)) // 4
+                    .signWith(SignatureAlgorithm.HS512, "secretkey").compact(); // 5
+            return token;
+        }
+        return "blad";
     }
+
+//    @PostMapping("/authorize")
+//    public String authorizeUser(@RequestBody LoginDto user){
+//        userService.login(user);
+//        return "wuitam";
+//    }
 }
