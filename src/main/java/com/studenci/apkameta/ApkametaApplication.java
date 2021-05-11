87b43f8e65a814d1cfa87a68b1cd2f6d0f49281a
mihau1211 <mihau1211@users.noreply.github.com>
2021-05-11 22:18:49 +0200

    Update ApkametaApplication.java

--- src/main/java/com/studenci/apkameta/ApkametaApplication.java
+++ src/main/java/com/studenci/apkameta/ApkametaApplication.java
@@ -2,12 +2,24 @@ package com.studenci.apkameta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class ApkametaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApkametaApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new JwtFilter());
+        filterRegistrationBean.setUrlPatterns(Collections.singleton("/user/*"));
+        return filterRegistrationBean;
     }
 
 }
