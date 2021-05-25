747be34d4e0d48578caddf3b9993f7a1e4cea1f3
mihau1211 <mihau1211@users.noreply.github.com>
2021-05-25 21:50:32 +0200

    Update ModuleControler.java

--- src/main/java/com/studenci/apkameta/modules/ModuleControler.java
+++ src/main/java/com/studenci/apkameta/modules/ModuleControler.java
@@ -4,18 +4,24 @@ import com.studenci.apkameta.users.LoginDto;
 import com.studenci.apkameta.users.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ModuleControler {
-    @Autowired
-    ModuleServiceImpl moduleService;
+//    @Autowired
+//    ModuleServiceImpl moduleService;
+//
+//    @PostMapping(value = "/modules")
+//    public void modules(@RequestBody LoginDto user){
+//        if (moduleService.verify(user))
+//            System.out.println("dziala");
+//    }
 
-    @PostMapping(value = "/modules")
-    public String modules(@RequestBody LoginDto user){
-        moduleService.verify(user);
-        return "ad";
+    @GetMapping("/modules")
+    public String hello(){
+        return "hello";
     }
 }
