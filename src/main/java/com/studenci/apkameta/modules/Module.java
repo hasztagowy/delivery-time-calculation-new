776bd347a6d0039262a995db632dd28e98aeb34a
mihau1211 <mihau1211@users.noreply.github.com>
2021-05-25 21:50:25 +0200

    Update Module.java

--- src/main/java/com/studenci/apkameta/modules/Module.java
+++ src/main/java/com/studenci/apkameta/modules/Module.java
@@ -1,14 +1,6 @@
 package com.studenci.apkameta.modules;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 public class Module {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private Long id;
     private String code;
     private Service[] services;
     private DeliveryWindow deliveryWindow;
