b78618159cadc86973c128e549808afb6e4d4b2e
mihau1211 <mihau1211@users.noreply.github.com>
2021-05-26 11:43:48 +0200

    Update ModuleService.java

--- src/main/java/com/studenci/apkameta/modules/ModuleService.java
+++ src/main/java/com/studenci/apkameta/modules/ModuleService.java
@@ -1,12 +1,10 @@
 package com.studenci.apkameta.modules;
 
 import com.studenci.apkameta.users.LoginDto;
-import com.studenci.apkameta.users.User;
-import org.springframework.stereotype.Service;
 
 
 public interface ModuleService {
 
     boolean verify(LoginDto user);
-    void createModule(Module module);
+    String createModule(Module module);
 }
