af8e5f1b84bd7302155deaf977f976d632f45672
mihau1211 <mihau1211@users.noreply.github.com>
2021-05-29 15:18:55 +0200

    Update DeliveryWindow.java

--- src/main/java/com/studenci/apkameta/DeliveryWindow/DeliveryWindow.java
+++ src/main/java/com/studenci/apkameta/DeliveryWindow/DeliveryWindow.java
@@ -1,11 +1,27 @@
 package com.studenci.apkameta.DeliveryWindow;
-
 import com.google.gson.GsonBuilder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.persistence.*;
+
+@Entity
 
 public class DeliveryWindow {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "from_1")
     private String from;
+    @Column(name = "to_1")
     private String to;
+
+    public DeliveryWindow() {
+    }
+
+    public DeliveryWindow(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
 
     public String getFrom() {
         return from;
