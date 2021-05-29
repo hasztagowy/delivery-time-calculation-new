24da402bd15daaa1b5ff728b4651e7d557f03540
WrumWrumm <83296669+WrumWrumm@users.noreply.github.com>
2021-05-29 14:18:39 +0200

    Update TransitTime.java

--- src/main/java/com/studenci/apkameta/transit/TransitTime.java
+++ src/main/java/com/studenci/apkameta/transit/TransitTime.java
@@ -1,15 +1,40 @@
 package com.studenci.apkameta.transit;
 
 import com.google.gson.GsonBuilder;
+import com.studenci.apkameta.Service.Service;
+import org.hibernate.annotations.IndexColumn;
 
+import javax.persistence.*;
+
+@Entity
 public class TransitTime {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "from_1")
     private String from;
+    @Column(name = "to_1")
     private String to;
 
+
+    @OneToOne(cascade = {CascadeType.ALL})
     private TransitTimeAvailability Dispatch;
+    @OneToOne(cascade = {CascadeType.ALL})
     private TransitTimeAvailability Transit;
+    @OneToOne(cascade = {CascadeType.ALL})
     private TransitTimeAvailability Delivery;
+
+
+    public TransitTime() {
+    }
+
+    public TransitTime(String from, String to, TransitTimeAvailability dispatch, TransitTimeAvailability transit, TransitTimeAvailability delivery) {
+        this.from = from;
+        this.to = to;
+        Dispatch = dispatch;
+        Transit = transit;
+        Delivery = delivery;
+    }
 
 
     public String getFrom() {
@@ -56,4 +81,7 @@ public class TransitTime {
     public String toString() {
         return new GsonBuilder().setPrettyPrinting().create().toJson(this);
     }
+
+
 }
+
