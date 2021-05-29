7ef73f92c4405253dd2a196bb86babe5fc97fa48
WrumWrumm <83296669+WrumWrumm@users.noreply.github.com>
2021-05-29 14:12:54 +0200

    Update Module.java

--- src/main/java/com/studenci/apkameta/modules/Module.java
+++ src/main/java/com/studenci/apkameta/modules/Module.java
@@ -1,15 +1,37 @@
 package com.studenci.apkameta.modules;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.google.gson.GsonBuilder;
 import com.studenci.apkameta.DeliveryWindow.DeliveryWindow;
 import com.studenci.apkameta.Service.Service;
+import com.studenci.apkameta.transit.TransitTime;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+@Entity
 public class Module {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String code;
-    private Service[] services;
+    @OneToOne(cascade = {CascadeType.ALL})
     private DeliveryWindow deliveryWindow;
+
+    @OneToMany(cascade = {CascadeType.ALL})
+    private List<Service> services = new ArrayList<>();
+
+    public Module() {
+    }
+
+    public Module(String code, DeliveryWindow deliveryWindow, List<Service> services) {
+        this.code = code;
+        this.deliveryWindow = deliveryWindow;
+        this.services = services;
+    }
 
     public String getCode() {
         return code;
@@ -17,14 +39,6 @@ public class Module {
 
     public void setCode(String code) {
         this.code = code;
-    }
-
-    public Service[] getServices() {
-        return services;
-    }
-
-    public void setServices(Service[] services) {
-        this.services = services;
     }
 
     public DeliveryWindow getDeliveryWindow() {
@@ -35,8 +49,17 @@ public class Module {
         this.deliveryWindow = deliveryWindow;
     }
 
+    public List<Service> getServices() {
+        return services;
+    }
+
+    public void setServices(List<Service> services) {
+        this.services = services;
+    }
+
     @Override
     public String toString() {
         return new GsonBuilder().setPrettyPrinting().create().toJson(this);
     }
 }
+
