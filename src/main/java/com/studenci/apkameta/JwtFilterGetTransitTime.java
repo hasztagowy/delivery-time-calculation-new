1f3a80fcaa5d928115f33cd4b9689f23db30f81d
mihau1211 <mihau1211@users.noreply.github.com>
2021-05-29 13:15:18 +0200

    Create JwtFilterGetTransitTime.java

---
+++ src/main/java/com/studenci/apkameta/JwtFilterGetTransitTime.java
@@ -0,0 +1,34 @@
+package com.studenci.apkameta;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class JwtFilterGetTransitTime implements javax.servlet.Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        String header = httpServletRequest.getHeader("authorization");
+
+        if (httpServletRequest == null || !header.startsWith("Bearer ")){
+            throw new ServletException("Wrong or empty header.");
+        } else {
+            Claims claims;
+            try {
+                String token = header.substring(7);
+                claims = Jwts.parser().setSigningKey("secretkey").parseClaimsJws(token).getBody();
+                servletRequest.setAttribute("claims", claims);
+            } catch (Exception e) {
+                throw new ServletException("Wrong key");
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
