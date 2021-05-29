b371662317c51101bd36e09fe2891b022b9f9bb1
WrumWrumm <83296669+WrumWrumm@users.noreply.github.com>
2021-05-29 14:14:05 +0200

    Update ModuleControler.java

--- src/main/java/com/studenci/apkameta/modules/ModuleControler.java
+++ src/main/java/com/studenci/apkameta/modules/ModuleControler.java
@@ -4,16 +4,16 @@ import com.studenci.apkameta.users.LoginDto;
 import com.studenci.apkameta.users.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ModuleControler {
 
     @Autowired
-    ModuleServiceImpl moduleService;
+    ModuleEntityRepository moduleEntityRepository;
+
+
+
 
 //    @PostMapping(value = "/modules")
 //    public void modules(@RequestBody LoginDto user){
@@ -22,12 +22,24 @@ public class ModuleControler {
 //    }
 
     @GetMapping("/modules")
+    public void helloo(){
+      //  Module newModule=new Module("kod");
+       // moduleEntityRepository.save(newModule);
+        System.out.println("hello");
+    }
+
+    @PostMapping("/modules")
+
+    String komunkat(@RequestBody Module newModule) {
+        moduleEntityRepository.save(newModule);
+        return "ok";
+    }
+
+    @DeleteMapping("/modules")
     public String hello(){
         return "hello";
     }
 
-    @PostMapping("/modules")
-    public String createModule(@RequestBody Module module){
-        return moduleService.createModule(module);
-    }
+
 }
+
