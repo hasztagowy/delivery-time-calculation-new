9ca635df3a58d4cd2e72a7be685705530148beed
mihau1211 <mihau1211@users.noreply.github.com>
2021-05-30 16:23:30 +0200

    Update TransitTimeController.java

--- src/main/java/com/studenci/apkameta/transit/TransitTimeController.java
+++ src/main/java/com/studenci/apkameta/transit/TransitTimeController.java
@@ -1,32 +1,34 @@
 package com.studenci.apkameta.transit;
 
 import com.studenci.apkameta.modules.Module;
+import com.studenci.apkameta.modules.ModuleEntityRepository;
+import com.studenci.apkameta.modules.ModuleEntityRepositoryImpl;
 import com.studenci.apkameta.users.UserControler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping
 public class TransitTimeController {
 
-//?serviceName&purchaseDate&fromCountry&toCountry
-//    @RequestMapping(value = "/{code}/transitTimes", method = RequestMethod.GET)
-//    public String getCode(@PathVariable("code") String code, @RequestParam String serviceName, @RequestParam String purchaseDate,
-//                   @RequestParam String fromCountry, @RequestParam String toCountry){
-//        if(code.equals("ee")){
-//            return "hej";
-//        }
-//
-//        return "dd";
-//    }
+    ModuleEntityRepositoryImpl moduleEntityRepositoryImpl;
+    public TransitTimeController(ModuleEntityRepositoryImpl moduleEntityRepositoryImpl){
+        this.moduleEntityRepositoryImpl = moduleEntityRepositoryImpl;
+    }
+    //?serviceName&purchaseDate&fromCountry&toCountry
 
+    @GetMapping("/{moduleCode}/transitTimes")
+    public String getCode(@PathVariable("moduleCode") String moduleCode){
 
-    @GetMapping(value = "/{moduleCode}/transitTimes")
-    public String getCode(@PathVariable("moduleCode") String moduleCode) {
-        if (moduleCode.equals("DHL")) {
-            return "moduleCode is DHL";
-        } else if(moduleCode.equals("GLS"))
-            return "moduleCode is GLS";
-        return "moduleCode is incorrect";
+        Module module = moduleEntityRepositoryImpl.getModuleByCode(moduleCode);
+
+        return module.toString();
+//        if (moduleCode.equals("DHL")) {
+//            return "moduleCode is "+module.getCode();
+//        } else if(moduleCode.equals("GLS"))
+//            return "moduleCode is GLS";
+//        return "moduleCode is incorrect";
     }
 
 
