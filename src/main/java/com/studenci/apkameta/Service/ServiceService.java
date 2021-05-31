e977e79f3d9f2e49a7c71395ed8888ed3ec49e1d
mihau1211 <mihau1211@users.noreply.github.com>
2021-05-31 11:58:26 +0200

    Create ServiceService.java

---
+++ src/main/java/com/studenci/apkameta/Service/ServiceService.java
@@ -0,0 +1,6 @@
+package com.studenci.apkameta.Service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceService extends JpaRepository<Service, Integer> {
+}
