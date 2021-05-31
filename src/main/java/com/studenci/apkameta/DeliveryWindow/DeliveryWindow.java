98c673a445db5a38a725bb145fa46481e8130bbd
mihau1211 <mihau1211@users.noreply.github.com>
2021-05-31 21:51:34 +0200

    Update DeliveryWindow.java

--- src/main/java/com/studenci/apkameta/DeliveryWindow/DeliveryWindow.java
+++ src/main/java/com/studenci/apkameta/DeliveryWindow/DeliveryWindow.java
@@ -39,6 +39,39 @@ public class DeliveryWindow {
         this.to = to;
     }
 
+    public String getFromM() {
+        String  minute = "";
+        for (int i=0; i<from.length(); i++) {
+            if (i==2 || i==3)
+                minute += from.charAt(i);
+        }
+        return minute;
+    }
+    public String getFromH() {
+        String hour = "";
+        for (int i=0; i<from.length(); i++) {
+            if (i==0 || i==1)
+                hour += from.charAt(i);
+        }
+        return hour;
+    }
+    public String getToM() {
+        String  minute = "";
+        for (int i=0; i<to.length(); i++) {
+            if (i==2 || i==3)
+                minute += to.charAt(i);
+        }
+        return minute;
+    }
+    public String getToH() {
+        String hour = "";
+        for (int i=0; i<to.length(); i++) {
+            if (i==0 || i==1)
+                hour += to.charAt(i);
+        }
+        return hour;
+    }
+
     @Override
     public String toString() {
         return new GsonBuilder().setPrettyPrinting().create().toJson(this);
